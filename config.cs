c2d7ee95c31730e96fbb20a3e5f4e89a02f62939
GeorgeFunner <33177533+Danikalyk@users.noreply.github.com>
2024-01-26 16:15:56 +0500

    Update config.cs
    Убраны под звёздочки логин и пароль для входа в бд

--- config.cs
+++ config.cs
@@ -2,6 +2,6 @@
 {
     public static class config
     {
-        public static string _connectionString = @"Data Source=ZT019\SQLEXPRESS;Initial Catalog=GAS_CUTTING;User ID=sa;Password=EmmojoyProoo123;Trust Server Certificate=True";
+        public static string _connectionString = @"Data Source=ZT019\SQLEXPRESS;Initial Catalog=GAS_CUTTING;User ID=**;Password=***********;Trust Server Certificate=True";
     }
 }
